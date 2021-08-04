9b76b364abd02684849d9819b26173ea6cd96b71
JonnyStef <jonathanstefanoni1999@gmail.com>
2021-08-04 15:21:46 +0200

    logo e layout

--- PetrisNetsGame/app/src/main/java/it/example/petrisnetsgame/MainActivity.java
+++ PetrisNetsGame/app/src/main/java/it/example/petrisnetsgame/MainActivity.java
@@ -3,12 +3,15 @@ package it.example.petrisnetsgame;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonSinglePlayer, buttonTwoPlayers;
+    boolean isPressed = false;
 
 
     @Override
@@ -25,6 +28,31 @@ public class MainActivity extends AppCompatActivity {
     public void buttonTwoPlayersOnCLick(View view) {
         Intent i = new Intent(this, PlayersActivity.class);
         startActivity(i);
+
+    }
+
+    public void onBackPressed() {
+        if(isPressed){
+            //quando si preme due volte indietro entro un secondo
+            //chiudi tutte le activity
+            finishAffinity();
+            System.exit(0);
+
+        }else{
+            //quando il doppio click ritarda per 2 secondi
+            //display toast
+            Toast.makeText(getApplicationContext(),
+                    R.string.click_to_exit, Toast.LENGTH_SHORT).show();
+
+            isPressed = true;
+        }
+
+        //inizializzo il runnable
+        Runnable runnable = () -> isPressed = false;
+
+        //handler delay per 2 secondi
+        new Handler().postDelayed(runnable, 2000);
+
 
     }
 }
